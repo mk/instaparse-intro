0b1fd2862eb08bc208c68a72db5164e3656ab8c1
Martin Kavalar <mk@katercalling.com>
2019-11-06 20:06:59 +0100

    add example

--- parsing.clj
+++ parsing.clj
@@ -1,58 +1,60 @@
 (ns parsing
   (:require [instaparse.core :as insta]))
 
+(+ 1 2 3)
+;; 1 + 2
 
-;; move = "left" | "right" | "up" | "down"
-;; expression =
-;; move
-;; | "repeat" natural "["  expression* "]"
-;; grammar = expression*
-;;
-;; also eine liste aus: einfachen move Anweisungen
-;; oder repeats mit einem count und einer liste von expressions, also z.b.
+(re-matches #"hello [a-z]+" "hello regex")
+
+(def word-parser
+  (insta/parser "sentence = word (<space> word)* <dot>
+                 space = #'\\s'
+                 dot = '.'
+                 word = #'[A-Za-z]+'"))
+
+
+(word-parser "hello instaparse.")
+(word-parser "hello instaparse")
+(word-parser "another.")
+(word-parser "my final sentence.")
+(word-parser "One more try. A final one.")
+
+
+
+
+
 
 (def p
   (insta/parser
-   "S = token (<space> token)*
-    <token> = move
+   "S = move (<space> move)*
     space = #'\\s'
     move = 'left' | 'right' | 'up' | 'down'"))
 
 (p "up")
 (p "down")
-(p "updownrightup")
 (p "up down right up")
+(p "updownrightup")
 
 (def whitespace
   (insta/parser "whitespace = #'\\s+'"))
 
 (def p
   (insta/parser
-   "S = token+
+   "<sentence> = token+
     <token> = move
     move = 'left' | 'right' | 'up' | 'down'"
    :auto-whitespace whitespace))
 
+(p " ")
 (p "up")
 (p "down")
 (p "updownrightup")
 (p "up down right up")
 
-(def p
-  (insta/parser
-   "S = expr (<space> expr)*
-    <expr> = move | repeat
-    repeat = 'repeat' <space> number <space> '[' <space> expr (<space> expr)* <space>']'
-    space = #'\\s'
-    number = #'[0-9]+'
-    move = 'left' | 'right' | 'up' | 'down'"))
-
-(p "down right repeat 2 [ up up right ] right")
-(p "down right repeat 2 [ up repeat 5 [ up right ] right ] right")
-
 
 (def p
   (insta/parser
+
    "S = expr+
     <expr> = move | repeat
     repeat = <'repeat'> number <'['>  expr+ <']'>
@@ -61,10 +63,24 @@
    :auto-whitespace whitespace))
 
 
-(p "repeat 2 [ up ]")
-(p "repeat 1 [ repeat 2 [ repeat 3 [ repeat 4 [ down ]]]]")
-(p "down right repeat 2 [ up up right ] right")
-(p "down right repeat 2 [ up repeat 5 [ up right ] right ] right")
+(p "up up")
+(p "repeat 2 [ up up ]")
+(p "repeat 1 [ repeat 2 [ repeat 3 [ down ]]]")
+(p "down left repeat 2 [ left ]")
+
+
+(def p+space
+  (insta/parser
+
+   "S = expr (<space> expr)*
+    <expr> = move | repeat
+    repeat = 'repeat' <space> number <space> <'['> <space> expr (<space> expr)* <space> <']'>
+    space = #'\\s'
+    number = #'[0-9]+'
+    move = 'left' | 'right' | 'up' | 'down'"))
+
+(p+space "down right repeat 2 [ up up right ] right")
+(p+space "down right repeat 2 [ up repeat 5 [ up right ] right ] right")
 
 
 (def move {"right" {:x 1}
@@ -77,12 +93,21 @@
 (move "down")
 
 
-(insta/transform {:number clojure.edn/read-string
-                  :move move
-                  :S (partial map +)} (p "down down down"))
+(insta/transform {:move move} (p "down down down"))
+
 
 (merge-with + {:x 1 :y 0} {:x 100 :y -99})
 (merge-with + (move "up") (move "up") (move "left"))
+
+(insta/transform {:move move
+                  :S (partial merge-with +)}
+                 (p "up up left up"))
+
+
+(insta/transform {:number clojure.edn/read-string}
+                 (p "repeat 2 [ down ]"))
+
+(clojure.edn/read-string "2")
 
 
 (defn ->pos [e]
@@ -91,33 +116,45 @@
                              :repeat (fn [times & expr]
                                        (apply concat (repeat times expr)))
                              :S (comp (partial apply merge-with +)
-                                      flatten)})))
-
-
-(apply concat (repeat 2 [:up :up]))
-
-(->pos "down up")
+                                      (fn [& args] (flatten args)))})))
+(->pos "down down")
 (->pos "left right")
-(->pos "right")
-(->pos "down")
-(->pos "repeat 1 [ repeat 1 [ repeat 1 [ up ]]]")
+(->pos "right down right")
+
+(->pos "down repeat 2 [ down ] down")
+
+(->pos "repeat 1 [ down up ]")
 (->pos "repeat 2 [ up left ]")
-(->pos "repeat 2 [ up repeat 2 [ up up ]]")
+(->pos "repeat 2 [ up repeat 2 [ left ]]")
 (->pos "repeat 3 [ left repeat 2 [ up ] ]")
 (->pos "repeat 1 [ repeat 2 [ repeat 3 [ repeat 4 [ down ]]]]")
 (->pos "repeat 3 [ left repeat 2 [ left right repeat 3 [ up ] ] ]")
 (->pos "repeat 3 [ left repeat 2 [ up ] ]")
 
-(insta/parses
- (insta/parser
-  "S = expr+
-    <expr> = repeat | move
-    number = #'[0-9]+'
-    move = 'left' | 'right' | 'up' | 'down'
-    repeat = <'repeat'> number <'['>  expr+ <']'>
-"
-  :auto-whitespace whitespace)
- "repeat 2 [ up repeat 3 [ left up up]]")
+
+(clojure.java.shell/sh "open" "http://instaparse-live.matt.is")
+(clojure.java.shell/sh "open" "http://nextjournal.com/dubroy/ohm-parsing-made-easy")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 (def arithmetic
